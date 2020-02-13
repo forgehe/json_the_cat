861e61eff13a8a6e0a59fe783df55fa3c12c9070
Richard Liao <liao098@gmail.com>
2020-02-13 21:31:41 +0000

    added initial breedFetcherTest.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,28 @@
 const request = require("request");
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
 
-request(url, (error, response, body) => {
-  if (error || response.statusCode !== 200) {
-    console.log(error);
-    console.log(response);
-    process.exit();
-  }
-  const data = JSON.parse(body);
-  console.log(data.length);
-  if (data.length === 0) {
-    console.log(`Breed not found :( `);
-  } else {
-    console.log(data);
-    console.log(typeof data);
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.log(error);
+      console.log(response);
+      process.exit();
+    }
+    const data = JSON.parse(body);
+    callback(data);
+  });
+};
+
+// const processBreed = breed => {
+//   console.log(breed.length);
+//   if (breed.length === 0) {
+//     console.log(`Breed not found :( `);
+//   } else {
+//     console.log(breed);
+//     console.log(typeof breed);
+//   }
+// };
+
+// fetchBreedDescription("american", processBreed);
+module.exports = { fetchBreedDescription };
