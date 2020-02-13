3d48307a149a8784a7681cf8d61bcb849aad8fc7
Richard Liao <liao098@gmail.com>
2020-02-13 22:06:18 +0000

    mocha testing with rudimentary error testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,13 +4,20 @@ const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
   request(url, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.log(error);
-      console.log(response);
-      process.exit();
-    }
+    // console.log(`error: ${typeof error}`);
+    // if (error || response.statusCode !== 200) {
+    //   console.log(error);
+    //   console.log(response);
+    //   process.exit();
+    // }
     const data = JSON.parse(body);
-    callback(data);
+    if (data.length === 0 || error) {
+      console.log(error);
+      callback(`error: ${error}`, null);
+    } else {
+      const data = JSON.parse(body);
+      callback(error, data[0].description);
+    }
   });
 };
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { fetchBreedDescription } = require("./breedFetcher");
 const breedName = process.argv[2];
 
 fetchBreedDescription(breedName, (error, desc) => {
+  // console.log(`error: ${error}, desc: ${desc}`);
   if (error) {
     console.log("Error fetch details:", error);
   } else {
